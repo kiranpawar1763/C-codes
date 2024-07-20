77d5e0c189b6f02879a1c2515d3955b794d7d9fc
Kiran Pawar <kiranpawar7662@gmail.com>
2024-07-20 14:20:36 +0530

    odd numbers

---
+++ if_else/OddNumber.c
@@ -0,0 +1,13 @@
+// print the odd number up to 100 //
+#include<stdio.h>
+int main()
+{
+    for( int i=0;i<=100;i++)
+    {
+        if(i%3==0)
+        printf("%d  ",i);
+
+    }
+    return 0;
+
+}
---
+++ if_else/evennum.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    for(int i=0;i<=100;i++)
+    {
+    if(i%4==0  )
+    printf("%d ",i);
+    
+    }
+    return 0;
+}
