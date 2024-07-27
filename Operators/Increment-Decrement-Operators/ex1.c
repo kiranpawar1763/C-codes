b14a98040b28421b3a8cb0c68ef1bb53a039ba99
Kiran Pawar <kiranpawar7662@gmail.com>
2024-07-27 23:38:06 +0530

    C practice codes

---
+++ Operators/Increment-Decrement-Operators/ex1.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+    int a=2,b;
+    b = a++ + a++ + a++;
+    printf("a=%d,b=%d\n",a,b);
+    a =2;
+    b = ++a + ++a + ++a;
+    printf("a=%d,b=%d",a,b);
+        return 0;
+}
+/*output by vs code:-
+a = 2
+here now we will go by right to left but actual answer will
+be printed from Left to right
+b = a++ + a++ + a++
+a = 5
+b = 9
+b = ++a + ++a + ++a
+a = 5
+b = 13
+*/
