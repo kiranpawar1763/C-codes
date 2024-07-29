0796c39122cde1187c72e463f6d056abd897868e
Kiran Pawar <kiranpawar7662@gmail.com>
2024-07-29 16:45:53 +0530

    C practice codes

---
+++ Operators/Increment-Decrement-Operators/ex6.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+void main()
+{
+    int a=50;
+    printf("%d\n",a++);
+    printf("%d\n",++a);
+}
+/*output:-
+a=
+b=
+condition 1:-
+a=50
+a++
+1.a++
+   50+1=51 
+note:-a = 50[becuse of post increment fist a value is 50 after print 50 to a++ and a=51 is go to next opretion]
+condition2:-
+a=51
+1.++a
+ 1+51=52
+  a=52
+    
+*/
