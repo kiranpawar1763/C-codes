edb5dc3c2a0699924fb31954f3a05a4df1bdd236
Kiran Pawar <kiranpawar7662@gmail.com>
2024-07-29 15:40:36 +0530

    C practice codes

---
+++ Operators/Increment-Decrement-Operators/ex2.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int a=11,b=8;
+    a++;
+    b--;
+    printf("a=%d,b=%d",a,b);
+
+}
+/*output 
+ a = 11
+ b = 8
+ condition
+ a++ ==> 11++ ==>12 
+ b-- ==>8--==>7
+ */
