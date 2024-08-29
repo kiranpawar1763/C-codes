d31b279dc2bf7c04802f06e78392f28b1d470f48
Kiran Pawar <kiranpawar7662@gmail.com>
2024-08-29 22:40:59 +0530

    sum of n terms of even natural numbers

---
+++ class lab/27 Aug 2024/ex3.c
@@ -0,0 +1,34 @@
+/*Write a C program to display the sum of n terms of even natural numbers.
+
+
+
+Test Data :
+
+Input number of terms : 5
+
+Expected Output :
+
+The even numbers are :2 4 6 8 10
+
+The Sum of even Natural Number upto 5 terms : 30
+*/
+#include<stdio.h>
+void main()
+{
+    int n ,sum=0,i;
+    printf("Enter the program:");
+    scanf("%d",&n);
+    printf("the even number are:");
+    for ( i=1;i<=n*2;i++)
+    {
+        if(i%2==0)
+        {
+        printf("%d ",i);
+        sum +=i;
+        }
+
+    }
+     printf("\nThe Sum of even Natural Number upto %d terms: %d", n, sum);
+
+        
+}
