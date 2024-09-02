7ea6b00ec75fd86dc33c061e9e30dfe3b9da3f39
Kiran Pawar <kiranpawar7662@gmail.com>
2024-09-02 21:57:45 +0530

    Reding Array and printing array elements

---
+++ class lab/practice code by class notes/class notes program  C lan/2 sep 2024-----Array/RedingAndPrintingArrayElements.c
@@ -0,0 +1,14 @@
+//Reading and printing array elements
+#include<stdio.h>
+void main()
+{
+    int a[100],n,i;
+    printf("Enter  Array size between 1-100");
+    scanf("%d",&n);
+    printf("Enter %d element ",n);
+    for(i=0;i<n;i++)
+    scanf("%d",&a[i]);
+    printf("Element");
+    for(i=0;i<=0;i++)
+    printf("%4d",i[a]);
+}
