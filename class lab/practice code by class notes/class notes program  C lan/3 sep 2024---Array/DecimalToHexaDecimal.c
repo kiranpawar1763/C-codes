7aab093ee1443f6402f673acbb6aec4785e8db4a
Kiran Pawar <kiranpawar7662@gmail.com>
2024-09-03 22:12:44 +0530

    Decimal to HexaDecimal

---
+++ class lab/practice code by class notes/class notes program  C lan/3 sep 2024---Array/DecimalToHexaDecimal.c
@@ -0,0 +1,22 @@
+//Decimal to octal
+
+#include<stdio.h>
+void main()
+{
+    int a[16]={0},n,i;
+    printf("Enter the no:");
+    scanf("%d",&n);
+    for(i=0;n!=0;i++,n=n/16)a[i]=n%16; //dec to oct
+    printf("Octal code");
+    for(i=15;i>=0;i--)
+    if(a[i]<10)
+    {
+     printf("%2d",a[i]);
+    
+    }
+    else
+    {
+        printf("%2c",87+a[i]);
+    }
+
+}
