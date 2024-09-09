917992c4e0a9a63e1d469d417892d9afbf897199
Kiran Pawar <kiranpawar7662@gmail.com>
2024-09-09 23:18:15 +0530

    merging of Array

--- c practice code by class notes/class notes program  C lan/5 sep 2024----Array/MergingofArrays.c
+++ c practice code by class notes/class notes program  C lan/5 sep 2024----Array/MergingofArrays.c
@@ -1,39 +1,36 @@
 #include<stdio.h>
 void main()
 {
-    int a[100],b[100],n1,n2,i,j;
+    int a[100],b[100],n1,n2,i,j, t=0;
     printf("Enter the 1st and 2nd Array size 1-100:");
     scanf("%d %d",&n1,&n2);
 
-    printf("Enter %d integers for 1st Array:",n1);
+    printf("Enter %d integers for 1st Array: ",n1);
     for(i=0;i<n1;i++)
     scanf("%d",&a[i]);
     
-    printf("Enter %d integers for 2nd Array",n2);
+    printf("Enter %d integers for 2nd Array: ",n2);
     for(i=0;i<n2;i++)
     scanf("%d",&b[i]);
+    
+    for(i=n1,j=0;i<n1+n2;i++,j++)
+    a[i]=b[j];
+
+    for(i=0; i<n1+n2; i++)
     {
-        for(i=n1,j=0;i<n1+n2;i++,j++)
-        a[i]=b[j];
-        for(i=0;i<n1+n2;i++)
+        for(j=i+1; j<n1+n2; j++)
         {
-            for(j=i+1;j<=n1+n2;i++)
+            if(a[i]<a[j])
             {
-                if(a[i]>a[j])
-                {
-                    int t=a[i];
-                    a[i]=a[j];
-                    a[j]=t;
-                }
+                t=a[i];
+                a[i]=a[j];
+                a[j]=t;
             }
         }
-        printf("Element are ");
-        for(i=0;i<n1+n2;i++)
-        printf("%4d",a[i]);
-        
     }
 
-
-
-
+    printf("Element are ");
+    for(i=0;i<n1+n2;i++)
+    printf("%4d",a[i]);
+    
 }
