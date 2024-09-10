6aefb9edf081e0b2990356bcb6cb790957cac466
Kiran Pawar <kiranpawar7662@gmail.com>
2024-09-10 21:47:37 +0530

    Transpose of n*n matrix

---
+++ c practice code by class notes/class notes program  C lan/9 aug 2024----2D Array/TransposeOfnmatrix.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+void main()
+{
+    int a[10][10] ,nr,nc,r,c;
+
+    printf("Enter the row And colmuns:");
+    scanf("%d %d",&nr,&nc);
+
+    printf("Enter %d elements:",nr*nc);
+    for(r=0;r<nr;r++)
+
+    for(c=0;c<nc;c++)
+    scanf("%d",&a[r][c]);
+
+    printf("transposed elements\n");
+    for(c=0;c<nc;c++)
+    {
+        for(r=0;r<nr;r++)
+        {
+            printf("%4d",a[r][c]);
+        }
+        printf("\n");
+    }
+}
