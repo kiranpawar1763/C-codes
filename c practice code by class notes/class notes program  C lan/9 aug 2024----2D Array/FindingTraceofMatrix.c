26934d4b88d74a0fc1aff8595793e27ddebf0266
Kiran Pawar <kiranpawar7662@gmail.com>
2024-09-10 15:19:01 +0530

    Finding trace of n*n matrix

---
+++ c practice code by class notes/class notes program  C lan/9 aug 2024----2D Array/FindingTraceofMatrix.c
@@ -0,0 +1,26 @@
+//Sum of principle diagonal elements is called trace.
+#include<stdio.h>
+void main()
+{
+    int a[10][10],nr,nc,r,c,s=0;
+    printf("Enter no of rows and columns:");
+    scanf("%d%d",&nr,&nc);
+    if(nr==nc)
+    {
+        printf("Enter %d elements",nr*nc);
+        for(r=0;r<nr;r++)
+        for(c=0;c<nr;c++)
+        {
+            scanf("%d",&a[r][c]);
+            if(r==c)
+             {
+                s+=a[r][c];
+             }
+        }
+        printf("Trace=%d",s);
+    }
+    else
+        {
+            printf("Row and columns should be same");
+        }
+}
