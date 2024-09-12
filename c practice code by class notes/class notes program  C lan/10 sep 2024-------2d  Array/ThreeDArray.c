01fe37b252f4d026fc9197c93616fd5c14290885
Kiran Pawar <kiranpawar7662@gmail.com>
2024-09-12 22:46:58 +0530

    Three D Array

---
+++ c practice code by class notes/class notes program  C lan/10 sep 2024-------2d  Array/ThreeDArray.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+    int a[2][2][2]={1,2,3,4,5,6,7,8},b,r,c;
+    for(b=0;b<2;b++)
+    {
+        printf("%d-Block\n",b);
+        for(r=0;r<2;r++)
+        {
+            for(c=0;c<2;c++)
+            printf("%4d",a[b][r][c]);
+            printf("\n");
+        }
+    }
+}
