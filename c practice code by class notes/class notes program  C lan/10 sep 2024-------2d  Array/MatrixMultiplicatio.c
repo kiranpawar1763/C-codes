2ad755c5553f850c4d5801c05339ae6d48de8fcb
Kiran Pawar <kiranpawar7662@gmail.com>
2024-09-12 22:39:10 +0530

    Matrix multipication

---
+++ c practice code by class notes/class notes program  C lan/10 sep 2024-------2d  Array/MatrixMultiplicatio.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+#include<stdio.h>
+void main()
+{
+    int a[10][10],b[10][10],nr,nc,r,c,s,k;
+
+  printf("Enter no of rows and columns ");
+  scanf("%d %d",&nr,&nc);
+
+   printf("Enter %d numbers for a array ",nr*nc);
+
+    for(r=0;r<nr;r++)
+    for(c=0;c<nc;c++)
+    scanf("%d",&a[r][c]);
+
+        printf("Enter %d numbers for b array ",nr*nc);
+        for(r=0;r<nr;r++)
+        for(c=0;c<nc;c++)
+        scanf("%d",&b[r][c]);
+
+    printf("Elements are");
+    printf("---------------------------------");
+    for(r=0;r<nr;r++)
+  {
+    for(c=0;c<nc;c++)
+    {
+        for(s=k=0;k<nc;k++)
+        {
+           s+=a[r][k]*b[k][c];
+        }
+         printf("%4d",s);
+
+   }
+       printf("\n");
+  }
+}
