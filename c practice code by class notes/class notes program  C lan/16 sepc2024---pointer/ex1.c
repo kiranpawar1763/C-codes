7121469d07ab759c3f6b4fcff2fb2acd8647551b
Kiran Pawar <kiranpawar7662@gmail.com>
2024-09-25 11:27:00 +0530

    Finding a variable value and address using a pointer:

---
+++ c practice code by class notes/class notes program  C lan/16 sepc2024---pointer/ex1.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+
+{
+    int a=100,*p=&a;
+    printf("a value is %d\n",a);
+    printf("a add is %u\n",&a);
+    printf("p value is %u\n",p);
+    printf("a value through p%d\n",*p);
+    *p =200;
+    printf("a=%d,*p=%d\n",a,*p);
+    a=300;
+    printf("a=%d,*p=%d\n",a,*p);
+}
