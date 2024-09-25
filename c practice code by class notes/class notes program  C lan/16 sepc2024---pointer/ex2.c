afa8d228cb0ca533446b6d8db449d4898f48be91
Kiran Pawar <kiranpawar7662@gmail.com>
2024-09-25 21:15:12 +0530

    Even or odd using pointer

---
+++ c practice code by class notes/class notes program  C lan/16 sepc2024---pointer/ex2.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+    int n,*p=&n;
+    printf("enter the number:");
+    scanf("%d",&n);
+    if(*p%2==0)
+    {
+        printf("even");
+    }
+    else
+    {
+        printf("odd");
+
+    }
+
+}
